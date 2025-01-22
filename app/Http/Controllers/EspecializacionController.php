258e7d4daa8de7982ce87b35d2c955ea8131fde3
JesCorAvi <jesuscorderoavila1@gmail.com>
2025-01-22 20:10:54 +0100

    Migraciones y modelos de especializaciones y familias

---
+++ app/Http/Controllers/EspecializacionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Especializacion;
+use App\Http\Requests\StoreEspecializacionRequest;
+use App\Http\Requests\UpdateEspecializacionRequest;
+
+class EspecializacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEspecializacionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Especializacion $especializacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Especializacion $especializacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEspecializacionRequest $request, Especializacion $especializacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Especializacion $especializacion)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/FamiliaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Familia;
+use App\Http\Requests\StoreFamiliaRequest;
+use App\Http\Requests\UpdateFamiliaRequest;
+
+class FamiliaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFamiliaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Familia $familia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Familia $familia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFamiliaRequest $request, Familia $familia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Familia $familia)
+    {
+        //
+    }
+}
