65523dd36c912222af84f45142921f8afcbff79a
José Antonio Dominguez <joseantonio.dominguez@iesdonana.org>
2025-01-30 15:28:54 +0100

    Ajustes seeder

--- database/factories/ObjetoFactory.php
+++ database/factories/ObjetoFactory.php
@@ -20,7 +20,7 @@ class ObjetoFactory extends Factory
             'denominacion' => $this->faker->word(1),  // Generates a two-word phrase
             'descripcion' => $this->faker->sentence(2),  // Generates an abstract image URL
             'stock' => $this->faker->numberBetween(1, 10),  // Random quantity between 1 and 10
-            'valor' => $this->faker->randomFloat(2, 1, 100), // Price between 1.00 and 100.00
+            'valor' => $this->faker->randomDigitNotZero(2, 1, 100), // Price between 1.00 and 100.00
         ];
     }
 }
